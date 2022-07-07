dd363331ae4fe767a026099fbb863a0d9bb88475
crazydj2 <crazydj2@naver.com>
2022-07-07 15:19:28 +0900

    add ArticleListItem.svelte & add ui template

--- src/view/main/ArticleList.svelte
+++ src/view/main/ArticleList.svelte
@@ -1,5 +1,16 @@
-<h1>리스트~~</h1>
+<LayoutGrid>
+    <Cell spanDevices={{ desktop: 12, tablet: 8, phone: 4 }}>
+        <div><h1>menu > menu > menu > ......~</h1></div>
+    </Cell>
+    {#each Array(39) as _unused, i}
+    <Cell spanDevices={{ desktop: 3, tablet: 4, phone: 4 }}>
+        <ArticleListItem i={i}></ArticleListItem>
+    </Cell>
+    {/each}
+</LayoutGrid>
 
 <script>
-	
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+    import ArticleListItem from '../child/ArticleListItem.svelte';
 </script>
